129b72751fde990b31aff13466d58910a9cf2d10
Mochammad Wildany Sihab <sihabwildan3@gmail.com>
2022-07-08 14:52:33 +0700

    implemented plugin

---
+++ src/api/notes/index.js
@@ -0,0 +1,11 @@
+import NotesHandler from './handler.js';
+import routes from './routes.js';
+
+export default {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const notesHandler = new NotesHandler(service, validator);
+    server.route(routes(notesHandler));
+  },
+};
---
+++ src/server.js
@@ -0,0 +1,30 @@
+import Hapi from '@hapi/hapi';
+import notes from './api/notes/index.js';
+import NotesService from './services/inMemory/NotesService.js';
+import NotesValidator from './validator/notes/index.js';
+
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
